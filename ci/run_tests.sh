f89565b80d2e8764d5bd4e807f6558f50105596f
Jan Bujak <j@exia.io>
2019-05-18 15:49:49 +0200

    Do not run tests for the bin crates on Travis

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -20,8 +20,6 @@ if [ "$IS_NIGHTLY" = "1" ]; then
 fi
 cargo test -p cli-core
 cargo test -p server-core
-cargo test -p memory-profiler-gather
-cargo test -p memory-profiler-cli
 
 if [ "$IS_NIGHTLY" = "1" ]; then
     ./ci/build_for_deployment.sh
