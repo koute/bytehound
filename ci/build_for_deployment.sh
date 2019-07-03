acdb6113b9fce8a8d59d70ff10c80eb8283cf8cd
Jan Bujak <jan.bujak@nokia.com>
2019-07-03 14:04:42 +0200

    Run the integration tests on stable too

--- ci/build_for_deployment.sh
+++ ci/build_for_deployment.sh
@@ -2,7 +2,21 @@
 
 set -euo pipefail
 
-cargo build --release --target=x86_64-unknown-linux-gnu -p memory-profiler
+set +e
+echo "$(rustc --version)" | grep -q "nightly"
+if [ "$?" = "0" ]; then
+    export IS_NIGHTLY=1
+else
+    export IS_NIGHTLY=0
+fi
+set -e
+
+if [ "$IS_NIGHTLY" = "1" ]; then
+    cargo build --release --target=x86_64-unknown-linux-gnu -p memory-profiler --features nightly
+else
+    cargo build --release --target=x86_64-unknown-linux-gnu -p memory-profiler
+fi
+
 cargo build --release --target=x86_64-unknown-linux-gnu -p memory-profiler-cli
 cargo build --release --target=x86_64-unknown-linux-gnu -p memory-profiler-gather
 
--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -7,27 +7,14 @@ TEST_SUBSET=${TEST_SUBSET:-0}
 
 export RUST_BACKTRACE=1
 
-set +e
-echo "$(rustc --version)" | grep -q "nightly"
-if [ "$?" = "0" ]; then
-    export IS_NIGHTLY=1
-else
-    export IS_NIGHTLY=0
-fi
-set -e
-
 if [[ "$TEST_SUBSET" == 0 || "$TEST_SUBSET" == 1 ]]; then
     cargo test -p common
-    if [ "$IS_NIGHTLY" = "1" ]; then
-        cargo test -p memory-profiler
-    fi
+    cargo test -p memory-profiler
     cargo test -p cli-core
     cargo test -p server-core
 fi
 
 if [[ "$TEST_SUBSET" == 0 || "$TEST_SUBSET" == 2 ]]; then
-    if [ "$IS_NIGHTLY" = "1" ]; then
-        ./ci/build_for_deployment.sh
-        cargo test -p integration-tests
-    fi
+    ./ci/build_for_deployment.sh
+    cargo test -p integration-tests
 fi
