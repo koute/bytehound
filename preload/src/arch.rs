7fc2c402ab079b14002ca1997912bfd6884b396a
Jan Bujak <jan.bujak@nokia.com>
2019-07-08 09:55:15 +0200

    Remove unnecessary `'static` lifetimes

--- preload/src/arch.rs
+++ preload/src/arch.rs
@@ -1,26 +1,26 @@
 #[cfg(target_arch = "x86")]
-pub const TARGET_ARCH: &'static str = "x86";
+pub const TARGET_ARCH: &str = "x86";
 
 #[cfg(target_arch = "x86_64")]
-pub const TARGET_ARCH: &'static str = "x86_64";
+pub const TARGET_ARCH: &str = "x86_64";
 
 #[cfg(target_arch = "mips")]
-pub const TARGET_ARCH: &'static str = "mips";
+pub const TARGET_ARCH: &str = "mips";
 
 #[cfg(target_arch = "mips64")]
-pub const TARGET_ARCH: &'static str = "mips64";
+pub const TARGET_ARCH: &str = "mips64";
 
 #[cfg(target_arch = "powerpc")]
-pub const TARGET_ARCH: &'static str = "powerpc";
+pub const TARGET_ARCH: &str = "powerpc";
 
 #[cfg(target_arch = "powerpc64")]
-pub const TARGET_ARCH: &'static str = "powerpc64";
+pub const TARGET_ARCH: &str = "powerpc64";
 
 #[cfg(target_arch = "arm")]
-pub const TARGET_ARCH: &'static str = "arm";
+pub const TARGET_ARCH: &str = "arm";
 
 #[cfg(target_arch = "aarch64")]
-pub const TARGET_ARCH: &'static str = "aarch64";
+pub const TARGET_ARCH: &str = "aarch64";
 
 #[cfg(target_endian = "little")]
 pub const IS_LITTLE_ENDIAN: bool = true;
