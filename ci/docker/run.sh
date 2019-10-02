f52f6d2f5f746e120d947f3354e1d16a284f682e
Jan Bujak <jan.bujak@nokia.com>
2019-10-02 10:32:05 +0200

    ci: Update docker scripts for interactive usage
    When used interactively the host's root directory will be mounted
    under `/mnt/host`, and the root password will be set to `root`.

--- ci/docker/run.sh
+++ ci/docker/run.sh
@@ -6,23 +6,27 @@ CWD="$(pwd)"
 cd "$(dirname $(readlink -f "$0"))"
 
 TARGET_LIST=$(rustup target list | grep "(installed)" | cut -d " " -f 1)
-
-docker build \
-    -f Dockerfile.hybrid \
-    -t crossenv \
-    --build-arg TARGET_LIST="$TARGET_LIST" \
-    --build-arg UID="$(id -u)" \
-    --build-arg GID="$(id -g)" \
-    ./
-
 EXTRA_ARGS=""
+IS_INTERACTIVE=0
+IMAGE_TAG=crossenv
 
 set +u
 if [ "$1" == "--interactive" ]; then
-    EXTRA_ARGS="$EXTRA_ARGS --entrypoint /bin/bash"
+    EXTRA_ARGS="$EXTRA_ARGS --entrypoint /bin/bash -v /:/mnt/host"
+    IS_INTERACTIVE=1
+    IMAGE_TAG=crossenv-interactive
     shift
 fi
 set -u
+
+docker build \
+    -f Dockerfile.hybrid \
+    -t $IMAGE_TAG \
+    --build-arg TARGET_LIST="$TARGET_LIST" \
+    --build-arg UID="$(id -u)" \
+    --build-arg GID="$(id -g)" \
+    --build-arg IS_INTERACTIVE="$IS_INTERACTIVE" \
+    ./
 
 docker run \
     --rm \
@@ -35,5 +39,5 @@ docker run \
     -v ~/.cargo/registry:/home/user/.cargo/registry \
     -v "$CWD:/home/user/cwd" \
     -w /home/user/cwd \
-    crossenv \
+    $IMAGE_TAG \
     "$@"
