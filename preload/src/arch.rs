026abe5a1d4cb1ef646d5d93708652cc82cc5617
Jan Bujak <j@exia.io>
2020-01-23 12:45:43 +0100

    Use `std::env::consts::ARCH` instead of defining our own constants

--- preload/src/arch.rs
+++ preload/src/arch.rs
@@ -1,26 +1,4 @@
-#[cfg(target_arch = "x86")]
-pub const TARGET_ARCH: &str = "x86";
-
-#[cfg(target_arch = "x86_64")]
-pub const TARGET_ARCH: &str = "x86_64";
-
-#[cfg(target_arch = "mips")]
-pub const TARGET_ARCH: &str = "mips";
-
-#[cfg(target_arch = "mips64")]
-pub const TARGET_ARCH: &str = "mips64";
-
-#[cfg(target_arch = "powerpc")]
-pub const TARGET_ARCH: &str = "powerpc";
-
-#[cfg(target_arch = "powerpc64")]
-pub const TARGET_ARCH: &str = "powerpc64";
-
-#[cfg(target_arch = "arm")]
-pub const TARGET_ARCH: &str = "arm";
-
-#[cfg(target_arch = "aarch64")]
-pub const TARGET_ARCH: &str = "aarch64";
+pub const TARGET_ARCH: &str = std::env::consts::ARCH;
 
 #[cfg(target_endian = "little")]
 pub const IS_LITTLE_ENDIAN: bool = true;
