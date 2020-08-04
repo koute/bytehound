8eba011e7940f35a962fc24606deeb4327d6b888
unpluggedcoder <unpluggedcoder@outlook.com>
2020-08-05 01:00:26 +0800

    RustFmt preload/src/init.rs

--- preload/src/init.rs
+++ preload/src/init.rs
@@ -8,14 +8,14 @@ use crate::utils::generate_filename;
 fn initialize_logger() {
     static mut SYSCALL_LOGGER: logger::SyscallLogger = logger::SyscallLogger::empty();
     static mut FILE_LOGGER: logger::FileLogger = logger::FileLogger::empty();
-    let log_level = if let Ok( value ) = env::var( "MEMORY_PROFILER_LOG" ) {
+    let log_level = if let Ok(value) = env::var("MEMORY_PROFILER_LOG") {
         match value.as_str() {
             "trace" => log::LevelFilter::Trace,
             "debug" => log::LevelFilter::Debug,
             "info" => log::LevelFilter::Info,
             "warn" => log::LevelFilter::Warn,
             "error" => log::LevelFilter::Error,
-            _ => log::LevelFilter::Off
+            _ => log::LevelFilter::Off,
         }
     } else {
         log::LevelFilter::Off
@@ -23,65 +23,70 @@ fn initialize_logger() {
 
     let pid = unsafe { libc::getpid() };
 
-    if let Ok( value ) = env::var( "MEMORY_PROFILER_LOGFILE" ) {
-        let path = generate_filename( &value, None );
-        let rotate_at = env::var( "MEMORY_PROFILER_LOGFILE_ROTATE_WHEN_BIGGER_THAN" ).ok().and_then( |value| value.parse().ok() );
+    if let Ok(value) = env::var("MEMORY_PROFILER_LOGFILE") {
+        let path = generate_filename(&value, None);
+        let rotate_at = env::var("MEMORY_PROFILER_LOGFILE_ROTATE_WHEN_BIGGER_THAN")
+            .ok()
+            .and_then(|value| value.parse().ok());
 
         unsafe {
-            if let Ok(()) = FILE_LOGGER.initialize( path, rotate_at, log_level, pid ) {
-                log::set_logger( &FILE_LOGGER ).unwrap();
+            if let Ok(()) = FILE_LOGGER.initialize(path, rotate_at, log_level, pid) {
+                log::set_logger(&FILE_LOGGER).unwrap();
             }
         }
     } else {
         unsafe {
-            SYSCALL_LOGGER.initialize( log_level, pid );
-            log::set_logger( &SYSCALL_LOGGER ).unwrap();
+            SYSCALL_LOGGER.initialize(log_level, pid);
+            log::set_logger(&SYSCALL_LOGGER).unwrap();
         }
     }
 
-    log::set_max_level( log_level );
+    log::set_max_level(log_level);
 }
 
 fn initialize_atexit_hook() {
-    info!( "Setting atexit hook..." );
+    info!("Setting atexit hook...");
     unsafe {
-        let result = libc::atexit( on_exit );
+        let result = libc::atexit(on_exit);
         if result != 0 {
-            error!( "Cannot set the at-exit hook" );
+            error!("Cannot set the at-exit hook");
         }
     }
 }
 
 fn initialize_signal_handlers() {
-    extern "C" fn sigusr_handler( signal: libc::c_int ) {
+    extern "C" fn sigusr_handler(signal: libc::c_int) {
         let signal_name = match signal {
             libc::SIGUSR1 => "SIGUSR1",
             libc::SIGUSR2 => "SIGUSR2",
-            _ => "???"
+            _ => "???",
         };
 
-        info!( "Signal handler triggered with signal: {} ({})", signal_name, signal );
+        info!(
+            "Signal handler triggered with signal: {} ({})",
+            signal_name, signal
+        );
         crate::global::toggle();
     }
 
     if opt::get().register_sigusr1 {
-        info!( "Registering SIGUSR1 handler..." );
+        info!("Registering SIGUSR1 handler...");
         unsafe {
-            libc::signal( libc::SIGUSR1, sigusr_handler as libc::sighandler_t );
+            libc::signal(libc::SIGUSR1, sigusr_handler as libc::sighandler_t);
         }
     }
 
     if opt::get().register_sigusr2 {
-        info!( "Registering SIGUSR2 handler..." );
+        info!("Registering SIGUSR2 handler...");
         unsafe {
-            libc::signal( libc::SIGUSR2, sigusr_handler as libc::sighandler_t );
+            libc::signal(libc::SIGUSR2, sigusr_handler as libc::sighandler_t);
         }
     }
 }
 
 pub fn startup() {
     initialize_logger();
-    info!( "Version: {}", env!( "CARGO_PKG_VERSION" ) );
+    info!("Version: {}", env!("CARGO_PKG_VERSION"));
 
     unsafe {
         opt::initialize();
@@ -94,6 +99,6 @@ pub fn startup() {
 
     initialize_signal_handlers();
 
-    env::remove_var( "LD_PRELOAD" );
-    info!( "Startup initialization finished" );
+    env::remove_var("LD_PRELOAD");
+    info!("Startup initialization finished");
 }
