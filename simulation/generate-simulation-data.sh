1f5650dffc874b8e61a7163eb91a8d4b972a1982
Jan Bujak <j@exia.io>
2021-08-18 06:31:48 +0000

    Preprocess the simulation data

--- simulation/generate-simulation-data.sh
+++ simulation/generate-simulation-data.sh
@@ -10,8 +10,14 @@ cd ..
 echo "Building the profiler..."
 cargo build -p bytehound-preload
 
+echo "Building the CLI..."
+cargo build -p bytehound-cli
+
 echo "Profiling the simulation..."
-export MEMORY_PROFILER_OUTPUT=simulation/memory-profiling-simulation.dat
+export MEMORY_PROFILER_OUTPUT=simulation/memory-profiling-simulation-raw.dat
 LD_PRELOAD=target/debug/libbytehound.so simulation/target/debug/simulation
+
+target/debug/bytehound postprocess --anonymize=partial -o simulation/memory-profiling-simulation.dat simulation/memory-profiling-simulation-raw.dat
+rm -f simulation/memory-profiling-simulation-raw.dat
 
 echo "Profiling data generated: memory-profiling-simulation.dat"
