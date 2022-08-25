ef8cb19a2fa520d40b1da23919f08527c00a228a
Jan Bujak <jan@parity.io>
2022-08-25 06:30:32 +0000

    Make `StreamingSerializer` take `FnOnce`

--- server-core/src/streaming_serializer.rs
+++ server-core/src/streaming_serializer.rs
@@ -1,31 +1,32 @@
 use serde::{Serialize, Serializer};
+use std::cell::RefCell;
 
 pub struct StreamingSerializer< F, R, T >
-    where F: Fn() -> R,
+    where F: FnOnce() -> R,
           R: Iterator< Item = T >
 {
-    callback: F
+    callback: RefCell< Option< F > >
 }
 
 impl< F, R, T > StreamingSerializer< F, R, T >
-    where F: Fn() -> R,
+    where F: FnOnce() -> R,
           R: Iterator< Item = T >,
           T: Serialize
 {
     pub fn new( callback: F ) -> Self {
-        StreamingSerializer { callback }
+        StreamingSerializer { callback: RefCell::new( Some( callback ) ) }
     }
 }
 
 impl< F, R, T > Serialize for StreamingSerializer< F, R, T >
-    where F: Fn() -> R,
+    where F: FnOnce() -> R,
           R: Iterator< Item = T >,
           T: Serialize
 {
     fn serialize< S: Serializer >( &self, serializer: S ) -> Result< S::Ok, S::Error > {
         use serde::ser::SerializeSeq;
 
-        let iter = (self.callback)();
+        let iter = (self.callback.borrow_mut().take().unwrap())();
         let mut seq = serializer.serialize_seq( None )?;
         for element in iter {
             seq.serialize_element( &element )?;
